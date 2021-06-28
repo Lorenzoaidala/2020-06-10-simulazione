a1be8422cb409e90077268bedd4f964643280e9a
Lorenzo Aidala <Lorenzo Aidala@DESKTOP-21723CJ.homenet.telecomitalia.it>
2021-06-28 16:34:40 +0200

    ----

--- src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
+++ src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
@@ -5,37 +5,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.imdb.model.Actor;
 import it.polito.tdp.imdb.model.Director;
 import it.polito.tdp.imdb.model.Movie;
 
 public class ImdbDAO {
-	
-	public List<Actor> listAllActors(){
+
+	public void listAllActors(Map<Integer,Actor> idMap){
 		String sql = "SELECT * FROM actors";
-		List<Actor> result = new ArrayList<Actor>();
+
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
+				if(!idMap.containsKey(res.getInt("id"))) {
+					Actor actor = new Actor(res.getInt("id"), res.getString("first_name"), res.getString("last_name"),
+							res.getString("gender"));
 
-				Actor actor = new Actor(res.getInt("id"), res.getString("first_name"), res.getString("last_name"),
-						res.getString("gender"));
-				
-				result.add(actor);
+					idMap.put(res.getInt("id"), actor);
+				}
 			}
 			conn.close();
-			return result;
-			
+
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
+
 		}
 	}
-	
+
 	public List<Movie> listAllMovies(){
 		String sql = "SELECT * FROM movies";
 		List<Movie> result = new ArrayList<Movie>();
@@ -48,19 +52,19 @@ public class ImdbDAO {
 
 				Movie movie = new Movie(res.getInt("id"), res.getString("name"), 
 						res.getInt("year"), res.getDouble("rank"));
-				
+
 				result.add(movie);
 			}
 			conn.close();
 			return result;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
+
+
 	public List<Director> listAllDirectors(){
 		String sql = "SELECT * FROM directors";
 		List<Director> result = new ArrayList<Director>();
@@ -72,21 +76,62 @@ public class ImdbDAO {
 			while (res.next()) {
 
 				Director director = new Director(res.getInt("id"), res.getString("first_name"), res.getString("last_name"));
-				
+
 				result.add(director);
 			}
 			conn.close();
 			return result;
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
-	
-	
-	
-	
+
+	public List<String> getGeneri(){
+		String sql="SELECT DISTINCT genre "
+				+ "FROM movies_genres "
+				+ "ORDER BY genre";
+		Connection conn = DBConnect.getConnection();
+		List<String> result = new LinkedList<>();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(res.getString("genre"));
+			}
+			conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+
+
+	}
+	public List<Actor> getVertici(Map<Integer,Actor> idMap,String genere){
+		String sql = "SELECT distinct actor_id "
+				+ "FROM roles r, movies_genres m "
+				+ "WHERE r.movie_id=m.movie_id "
+				+ "AND genre =?";
+		Connection conn = DBConnect.getConnection();
+		List<Actor> result = new LinkedList<>();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, genere);
+			ResultSet res = st.executeQuery();
+			while(res.next()) {
+				if(idMap.containsKey(res.getInt("actor_id"))) {
+					result.add(idMap.get(res.getInt("actor_id")));
+				}
+			}
+			conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return result;
+	}
+
 }
--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,5 +1,26 @@
 package it.polito.tdp.imdb.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.imdb.db.ImdbDAO;
+
 public class Model {
 
+	private Map<Integer, Actor> idMap;
+	private ImdbDAO dao;
+	private Graph<Actor,DefaultWeightedEdge> grafo;
+	
+	public Model() {
+		dao = new ImdbDAO();
+		idMap = new HashMap<>();
+		dao.listAllActors(idMap);
+	}
+	public List<String> getGeneri(){
+		return dao.getGeneri();
+	}
 }
